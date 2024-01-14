e917a8c2e6cdbadd25ccb4a20571223cc762dce0
plescaevelyn <plescaevelyn@yahoo.com>
2024-01-14 20:13:28 +0200

    current state

--- RCS/project/scripts/object_placement_doosan.py
+++ RCS/project/scripts/object_placement_doosan.py
@@ -11,31 +11,43 @@ import rospy
 import os
 import threading, time
 import sys
-import DR_init
-from DSR_ROBOT import *
-
 sys.dont_write_bytecode = True
-sys.path.append( os.path.abspath(os.path.join(os.path.dirname(__file__),"../../../../common/imp")) ) # get import path : DSR_ROBOT.py 
+sys.path.append('/home/plescaevelyn/catkin_ws/src/y4-IA/RCS/project/scripts')
+sys.path.append( os.path.abspath(os.path.join(os.path.dirname(__file__),"../../../../doosan-robot/common/imp")) ) # get import path : DSR_ROBOT.py 
+
+# defining a single robot
+ROBOT_ID     = "dsr01"
+ROBOT_MODEL  = "m1013"
+import DR_init
+DR_init.__dsr__id = ROBOT_ID
+DR_init.__dsr__model = ROBOT_MODEL
+from DSR_ROBOT import *
 
 # Global variables for ROS service proxies and publisher
 pub_stop = None
 srv_robotiq_2f_open = None
 srv_robotiq_2f_move = None
 
-# Initialise robot
-ROBOT_ID     = "dsr01"
-ROBOT_MODEL  = "m1013"
-DR_init._dsr_id = ROBOT_ID
-DR_init._dsr_model = ROBOT_MODEL
-
 # Define functions for opening and closing the gripper
 def gripper_grasp(width):
     global srv_robotiq_2f_move
-    srv_robotiq_2f_move(width) # close the gripper with a width of 0.1 - 0.8 units
+    
+    # close the gripper with a width of 0.1 - 0.8 units
+    try:
+        # Attempt to call the service
+        srv_robotiq_2f_move(width)
+    except rospy.ServiceException as e:
+        rospy.logerr("Service call failed: %s", e) 
     
 def gripper_release():
     global srv_robotiq_2f_open
-    srv_robotiq_2f_open() # open the gripper
+    
+    # open the gripper
+    try:
+        # Attempt to call the service
+        srv_robotiq_2f_open()
+    except rospy.ServiceException as e:
+        rospy.logerr("Service call failed: %s", e)
 
 def SET_ROBOT(id, model):
     ROBOT_ID = id; ROBOT_MODEL= model   
@@ -96,7 +108,13 @@ if __name__ == "__main__":
     rospy.init_node('pick_and_place_simple_py')
     rospy.on_shutdown(shutdown)
 
-    pub_stop = rospy.Publisher('/'+ROBOT_ID +ROBOT_MODEL+'/stop', RobotStop, queue_size=10)    
+    # Check if the gripper open service is available
+    rospy.wait_for_service('/' + ROBOT_ID + ROBOT_MODEL + '/gripper/robotiq_2f_open', timeout=None)
+    # Check if the gripper move service is available
+    rospy.wait_for_service('/' + ROBOT_ID + ROBOT_MODEL + '/gripper/g', timeout=None)
+
+    pub_stop = rospy.Publisher('/'+ROBOT_ID+ROBOT_MODEL+'/stop', RobotStop, queue_size=10)
+
     srv_robotiq_2f_open = rospy.ServiceProxy('/' + ROBOT_ID + ROBOT_MODEL + '/gripper/robotiq_2f_open', Robotiq2FOpen)       
     srv_robotiq_2f_move = rospy.ServiceProxy('/' + ROBOT_ID + ROBOT_MODEL + '/gripper/g', Robotiq2FMove)
 
---
+++ RCS/project/scripts/test.py
@@ -0,0 +1,1 @@
+import DSR_ROBOT
